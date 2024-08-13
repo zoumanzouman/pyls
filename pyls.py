e400ac0aa3b8ee92bce1b436e6a5736590fe952c
zouman <zoumanhashim00@gmail.com>
2024-08-13 17:14:50 +0530

    added features, removal of test files, addressed all isues. final draft submission

--- pyls.py
+++ pyls.py
@@ -2,33 +2,30 @@ import os
 import time
 import argparse
 
+#this checks whether it is a file or directory and returns either true or false
+def directory_checker(file_path):
+  return {
+      'is_file': os.path.isfile(file_path),
+      'is_dir': os.path.isdir(file_path),
+  }
+    
+#this retrieves information regarding the file/path
 def get_file_info(file_path):
-  """Gets file information including modification time and size.
-
-  Args:
-      file_path (str): Path to the file.
-
-  Returns:
-      dict: Dictionary containing modification time, size, and is_dir flag.
-  """
   stat = os.stat(file_path)
   return {
       "mtime": time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(stat.st_mtime)),
-      "size": stat.st_size if os.path.isfile(file_path) else 0,
-      "is_dir": os.path.isdir(file_path),
+      "size": stat.st_size 
   }
 
+#this lists files and folders according to the input, -l, -F, -h
 def list_files_and_folders(args):
-  """Lists files and folders in the current working directory.
-
-  Args:
-      args (argparse.Namespace): Parsed arguments.
-  """
+    
   for item in os.listdir():
     full_path = os.path.join(os.getcwd(), item)
     if args.long:
       file_info = get_file_info(full_path)
       print(f"{file_info['mtime']} {file_info['size']} {item}")
+      
     else:
       if args.format:
         if os.path.isdir(full_path):
